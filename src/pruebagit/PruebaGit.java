e659a0f5f33c46e1549eae8836082110776bf9d8
MarcosMadrid <daw202314@estudiantes.salesianasnsp.es>
2022-03-22 09:00:39 +0100

    Primera parte nombrecito

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -17,7 +17,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Esto es una prueba GIT");
+        System.out.println("Mi nombre es pola gorda");
     }
     
 }
