ff4bd0c2c6bbb6126f3c45cdc526c427c2fd78df
MarcosMadrid <daw202314@estudiantes.salesianasnsp.es>
2022-03-28 11:49:56 +0200

    Pratica13_commit

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -17,7 +17,8 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Mi nombre es paula");
+        System.out.println("Mi nombre es adolfo cenicienta de la mancha toro gutierrez enmascarado");
+        
     }
     
 }
